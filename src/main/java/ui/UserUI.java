1c1dfee9a1adcf3182d9be61f47a5522864ca6c4
romeo <eduram_mt@yahoo.es>
2021-06-03 18:08:29 -0400

    increment task,ui,test mejorado

--- src/main/java/ui/UserUI.java
+++ src/main/java/ui/UserUI.java
@@ -4,8 +4,5 @@ import org.openqa.selenium.By;
 
 public class UserUI {
     public static By form = By.id("formUser");
-<<<<<<< HEAD
-=======
     public static By name = By.xpath("//a[.='Basic Auth']");
->>>>>>> ba3716e (increment task,ui,test)
 }
